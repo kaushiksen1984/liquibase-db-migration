9a9aaa99d92842af2b0b341100827b7df72e9640
Kaushik Sen <kaushik.extc@gmail.com>
2022-08-26 12:18:20 -0500

    Initial Commit

---
+++ src/main/java/com/liquibase/api/LiquibaseDbMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.liquibase.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDbMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDbMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liquibase/api/LiquibaseDbMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquibase.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDbMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
